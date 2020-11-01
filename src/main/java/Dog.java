6353f08d316870b48f4b40b7e4eccd6badccf1ab
nmakdessi <21nmakdessi@athenian.org>
2020-11-01 01:11:15 +0000

    Still workiing

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,18 +1,41 @@
 public class Dog extends Pet {
     // Instance variable(s)
+    private String breed;
 
 
     // Constructors
+    public Dog(String name, int age, String breed);{
+        super(a, b);
+        this.breed = breed;
+    }
+
+    public Dog(){
+        super("steve", 2);
+        this.breed = "poodle";
+    }
     
 
     // makeNoise() method
-
+    public void makeNoise() {
+        System.out.println("Woof!");
+    }
 
     // toString method
-
+    public String toString() {
+        String str = this.getName() + " " + this.getAge() + " " + this.getBreed();
+        return str;
+    }
 
     // Getter
-
+    public String getBreed(){
+        return this.breed;
+    }
 
     // Setter
+    public void setBreed(String breed) {
+        if (breed.trim().length() != 0) {
+            this.breed = breed.trim;
+        }
+    }
+
 }
--- src/main/java/DogClient.java
+++ src/main/java/DogClient.java
@@ -1,5 +1,9 @@
 public class DogClient {
     public static void main(String[] args) {
-        
+        Dog p2 = new Dog("Steve", 7, "Poodle");
+        System.out.println(p2);
+        System.out.println(p2.getBreed());
+        p2.makeNoise(); // woof
+        System.out.println(new Dog());
     }
 }
